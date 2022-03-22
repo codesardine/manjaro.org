66151c42e9c305b02b995bac3e02967cea60dbf8
Vitor Lopes <vmnlop@gmail.com>
2022-03-22 12:17:39 +0000

    add yoast to blogpages

--- blog/models.py
+++ blog/models.py
@@ -16,6 +16,9 @@ from modelcluster.fields import ParentalKey, ParentalManyToManyField
 from modelcluster.contrib.taggit import ClusterTaggableManager
 from taggit.models import TaggedItemBase
 
+from wagtail.admin.edit_handlers import TabbedInterface, ObjectList
+from wagtailyoast.edit_handlers import YoastPanel
+
 
 class BlogIndexPage(Page):
     intro = RichTextField(blank=True)
@@ -101,6 +104,20 @@ class BlogPage(Page):
         FieldPanel('content'),
         InlinePanel('gallery_images', label="Gallery images"),
     ]
+
+    keywords = models.CharField(default='', blank=True, max_length=100)
+
+    edit_handler = TabbedInterface([
+        ObjectList(content_panels, heading=('Content')),
+        ObjectList(Page.promote_panels, heading=('Promote')),
+        ObjectList(Page.settings_panels, heading=('Settings')),
+        YoastPanel(
+            keywords='keywords',
+            title='seo_title',
+            search_description='search_description',
+            slug='slug'
+        ),
+    ])
 
 
 class BlogPageGalleryImage(Orderable):
