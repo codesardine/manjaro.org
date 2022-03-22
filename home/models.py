69ec449785863d662602ea2f1959f988c3083431
Vitor Lopes <vmnlop@gmail.com>
2022-03-22 12:11:14 +0000

    add ability to build pages under home

--- home/models.py
+++ home/models.py
@@ -10,14 +10,14 @@ from wagtail.core.fields import StreamField
 from wagtail.admin.edit_handlers import TabbedInterface, ObjectList
 from wagtailyoast.edit_handlers import YoastPanel
 
-class HomePage(Page):
-    max_count=1
-    template = "home/home_page.html"
-    subpage_types = ['home.HomePage']
+
+class WebPage(Page):
+    template = "home/web-page.html"
+    subpage_types = ['home.WebPage']
     parent_page_types = [
         'home.HomePage'
     ]
-    
+
     content = StreamField(
         [
             ("richtext", blocks.RichtextBlock()),
@@ -44,4 +44,42 @@ class HomePage(Page):
             slug='slug'
         ),
     ])
+
+
+class HomePage(Page):
+    max_count=1
+    template = "home/home-page.html"
+    subpage_types = ['home.WebPage']
+    parent_page_types = [
+        'wagtailcore.Page'
+    ]
+
+    content = StreamField(
+        [
+            ("richtext", blocks.RichtextBlock()),
+            ("product_details", blocks.ProductBlock()),        
+        ],
+        null=True,
+        blank=True,
+    )
+
+    content_panels = Page.content_panels + [
+        StreamFieldPanel("content"),
+    ]
+
+    keywords = models.CharField(default='', blank=True, max_length=100)
+
+    edit_handler = TabbedInterface([
+        ObjectList(content_panels, heading=('Content')),
+        ObjectList(Page.promote_panels, heading=('Promote')),
+        ObjectList(Page.settings_panels, heading=('Settings')),
+        YoastPanel(
+            keywords='keywords',
+            title='seo_title',
+            search_description='search_description',
+            slug='slug'
+        ),
+    ])
+    
+    
     
