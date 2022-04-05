a88df1576dfc03374d75d8c81946fa72dccbd553
Vitor Lopes <vmnlop@gmail.com>
2022-04-05 18:01:18 +0100

    add yost downloads

--- home/models.py
+++ home/models.py
@@ -69,6 +69,21 @@ class Downloads(Page):
         context['data'] = data
         return context
 
+
+    keywords = models.CharField(default='', blank=True, max_length=100)
+
+    edit_handler = TabbedInterface([
+        ObjectList(content_panels, heading=('Content')),
+        ObjectList(Page.promote_panels, heading=('Promote')),
+        ObjectList(Page.settings_panels, heading=('Settings')),
+        YoastPanel(
+            keywords='keywords',
+            title='seo_title',
+            search_description='search_description',
+            slug='slug'
+        ),
+    ])
+
 #Downloads._meta.get_field("title").default = "Downloads"
 #Downloads._meta.get_field("slug").default = "default-homepage-title"
 
