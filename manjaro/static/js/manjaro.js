f4ca4166b52ff13e4907e61d40b88ceec841a44b
Vitor Lopes <vmnlop@gmail.com>
2022-04-08 16:32:59 +0100

    rework mobile menu

---
+++ manjaro/static/js/manjaro.js
@@ -0,0 +1,12 @@
+const btnCloseMenu = document.getElementById("btn-close-menu")
+const btnOpenMenu = document.getElementById("btn-open-menu")
+
+btnCloseMenu.addEventListener("click", function() {
+    const menu = document.getElementById("menu")
+    menu.classList.add("hidden")
+})
+
+btnOpenMenu.addEventListener("click", function() {
+    const menu = document.getElementById("menu")
+    menu.classList.remove("hidden")
+})
