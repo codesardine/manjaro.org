08466f5e678ab1451b570ccbfcf23d6da30071e8
Vitor Lopes <vmnlop@gmail.com>
2022-04-11 17:50:12 +0100

    add link highligter mouse effect

--- manjaro/static/js/manjaro.js
+++ manjaro/static/js/manjaro.js
@@ -10,3 +10,23 @@ btnOpenMenu.addEventListener("click", function() {
     const menu = document.getElementById("menu")
     menu.classList.remove("hidden")
 })
+
+const cursor = document.getElementById("cursor");
+document.addEventListener("mousemove", function(e) {
+    cursor.style.left = e.clientX + "px",
+    cursor.style.top = e.clientY + "px";
+});
+
+
+const cursorHover = document.querySelectorAll("a");
+cursorHover.forEach(a => {
+    a.addEventListener("mouseover", function(e) {
+        cursor.classList.add("enlarge")
+    })
+  });
+
+cursorHover.forEach(a => {
+    a.addEventListener("mouseout", function(e) {
+        cursor.classList.remove("enlarge")
+    })
+});
