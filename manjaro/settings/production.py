1bacf01ee4e8395e5a2ce19928007286a57f1dbe
Vitor Lopes <vmnlop@gmail.com>
2022-04-19 08:40:51 +0100

    add prod env vars

--- manjaro/settings/production.py
+++ manjaro/settings/production.py
@@ -1,6 +1,24 @@
 from .base import *
+import os
+env = os.environ.copy()
 
-DEBUG = False
+SECRET_KEY = env("SECRET_KEY")
+DEBUG = env("DEBUG")
+ALLOWED_HOSTS = env("DJANGO_ALLOWED_HOSTS").split(" ")
+
+DATABASES = {
+    'default': {
+        'ENGINE': env("SQL_ENGINE"),
+        'NAME': env("SQL_DATABASE"),
+        'USER': env("SQL_USER"),
+        'PASSWORD': env("SQL_PASSWORD"),
+        'HOST': env("SQL_HOST"),
+        'PORT': env("SQL_PORT"),
+    }
+}
+
+RECAPTCHA_PUBLIC_KEY = env['RECAPTCHA_PUBLIC_KEY']
+RECAPTCHA_PRIVATE_KEY = env['RECAPTCHA_PRIVATE_KEY']
 
 try:
     from .local import *
