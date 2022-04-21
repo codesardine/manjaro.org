075290cd416f08d5e62a60eb8050e26664976f76
Vitor Lopes <vmnlop@gmail.com>
2022-04-21 11:50:53 +0100

    try to fix whitenoise

--- manjaro/wsgi.py
+++ manjaro/wsgi.py
@@ -1,6 +1,8 @@
 import os
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
+from manjaro.settings.base import STATIC_ROOT
+from whitenoise import WhiteNoise
+
 
 #os.environ.setdefault("DJANGO_SETTINGS_MODULE", "manjaro.settings.production")
 
@@ -8,4 +10,4 @@ application = get_wsgi_application()
 
 SETTINGS = os.environ.get("DJANGO_SETTINGS_MODULE")
 if SETTINGS == "manjaro.settings.production":
-    application = DjangoWhiteNoise(application)
+    application = WhiteNoise(application, root=STATIC_ROOT)
