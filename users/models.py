a82ba98be7f46d8e826498d76e37a698a55b042f
Vitor Lopes <vmnlop@gmail.com>
2022-04-23 18:18:28 +0100

    fix #23

--- users/models.py
+++ users/models.py
@@ -3,10 +3,11 @@ from django.db import models
 
 
 class User(AbstractUser):
-    description = models.TextField(max_length=150, blank=True)
+    description = models.TextField(max_length=350, blank=True)
+    bio = models.TextField(max_length=350, blank=True)
     title = models.CharField(max_length=50, blank=True)
-    tweeter = models.URLField(max_length=100, blank=True)
-    github = models.URLField(max_length=100, blank=True)
+    tweeter = models.URLField(blank=True)
+    github = models.URLField(blank=True)
     avatar = models.ForeignKey(
         'wagtailimages.Image',
         null=True,
@@ -14,5 +15,3 @@ class User(AbstractUser):
         on_delete=models.SET_NULL,
         related_name='+',
     )
-    bio = models.TextField(max_length=250, blank=True)
-
