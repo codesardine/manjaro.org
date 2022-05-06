7ee0acad58f85258b158897b3ab23322efeb417d
Vitor Lopes <vmnlop@gmail.com>
2022-05-06 15:38:57 +0100

    fix #41

--- manjaro/settings/base.py
+++ manjaro/settings/base.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'wagtail.contrib.modeladmin',
+    'django.contrib.sitemaps',
 ]
 
 MIDDLEWARE = [
--- manjaro/urls.py
+++ manjaro/urls.py
@@ -8,6 +8,7 @@ from wagtail.core import urls as wagtail_urls
 from wagtail.documents import urls as wagtaildocs_urls
 from puput import urls as puput_urls 
 from search import views as search_views
+from wagtail.contrib.sitemaps.views import sitemap
 
 urlpatterns = [
     path('django-admin/', admin.site.urls),
@@ -16,26 +17,19 @@ urlpatterns = [
     path('documents/', include(wagtaildocs_urls)),
 
     path('search/', search_views.search, name='search'),
-    path("__reload__/", include("django_browser_reload.urls")),
     path(r'', include('puput.urls')),
 ] 
 
 if settings.DEBUG:
     from django.conf.urls.static import static
     from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-
-    # Serve static and media files from development server
+    
+    urlpatterns += path("__reload__/", include("django_browser_reload.urls")),
     urlpatterns += staticfiles_urlpatterns()
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 urlpatterns = urlpatterns + [
-    # For anything not caught by a more specific rule above, hand over to
-    # Wagtail's page serving mechanism. This should be the last pattern in
-    # the list:
+    path('sitemap.xml', sitemap),
     path(r'',include(puput_urls)),
     path("", include(wagtail_urls)),
-
-    # Alternatively, if you want Wagtail pages to be served from a subpath
-    # of your site, rather than the site root:
-    #    path("pages/", include(wagtail_urls)),
 ]
