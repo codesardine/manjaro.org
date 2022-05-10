5bf3fb39cfc29de7b5666b386defdbd0c1f73354
Vitor Lopes <vmnlop@gmail.com>
2022-05-10 11:36:19 +0100

    disable static

--- manjaro/urls.py
+++ manjaro/urls.py
@@ -1,6 +1,5 @@
 from django.conf import settings
 from django.urls import include, path
-from django.conf.urls.static import static
 from django.contrib import admin
 
 from wagtail.admin import urls as wagtailadmin_urls
@@ -21,6 +20,7 @@ urlpatterns = [
 ] 
 
 if settings.DEBUG:
+    pass
     from django.conf.urls.static import static
     from django.contrib.staticfiles.urls import staticfiles_urlpatterns
     
