180be67ce9c65881ca84492dff40d6e823def6c8
Vitor Lopes <vmnlop@gmail.com>
2022-05-16 19:48:37 +0100

    rename path

--- manjaro/settings/base.py
+++ manjaro/settings/base.py
@@ -149,7 +149,7 @@ WHITENOISE_MANIFEST_STRICT = False
 
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
