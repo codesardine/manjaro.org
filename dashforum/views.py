fe34e90bfc9c9d6ab325363cc6d5b3204cb10230
papajoker <papajoke@manjaro.fr>
2022-06-26 20:47:59 +0200

    [admin] display ssl certifications

--- dashforum/views.py
+++ dashforum/views.py
@@ -1,9 +1,43 @@
 import urllib.request
+from urllib.request import ssl, socket
+import datetime
 import json
 import concurrent.futures
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
+def certificates(request):
+    hostname = 'manjaro.org'
+    port = '443'
+    dns = (
+        "aur", "blog", "download", "forum",
+        "mirrors", "packages", "ping",
+        "software", "wiki", "www",
+    )
+    dns = sorted(set(dns))
+    now = datetime.datetime.now()
+    certificates = []
+
+    for url in dns:
+        url = f"{url}.{hostname}"
+
+        ssl_context = ssl.create_default_context()
+        with socket.create_connection((url, port)) as sock:
+            with ssl_context.wrap_socket(sock, server_hostname=url) as ssock:
+                certificate = ssock.getpeercert()
+                certExpires = datetime.datetime.strptime(certificate['notAfter'], '%b %d %H:%M:%S %Y %Z')
+                delta = certExpires - now
+                certificates.append({
+                    "url": url,
+                    "date": certExpires,
+                    "delta": delta.days
+                })
+
+
+    return render(request, 'wagtailforum/certifs.html', {
+        'certifs': certificates,
+    })
 
 def index(request):
 
@@ -22,7 +56,7 @@ def index(request):
     with concurrent.futures.ThreadPoolExecutor(max_workers=12) as executor:
         futures.append(executor.map(_get_subject, topics))
 
-    return render(request, 'wagtailforum/index.html', {
+    return render(request, 'wagtailforum/certifs.html', {
         'branch': branch,
         'topics': topics,
     })
