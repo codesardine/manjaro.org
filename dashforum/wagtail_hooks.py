8d917dc110339ac986819b20fc262d32f34bb896
Vitor Lopes <vmnlop@gmail.com>
2022-07-08 09:43:18 +0100

    fix #91

--- dashforum/wagtail_hooks.py
+++ dashforum/wagtail_hooks.py
@@ -1,19 +1,14 @@
 from django.urls import path, reverse
 from wagtail.admin.menu import MenuItem
 from wagtail.core import hooks
-from .views import certificates, index
+from .views import certificates
 
 
 @hooks.register('register_admin_urls')
-def register_forum_url():
+def register_cert_url():
     return [
-        path('forum/', index, name='forum'),
         path('certificates/', certificates, name='certificates'),
     ]
-
-@hooks.register('register_admin_menu_item')
-def register_forum_menu_item():
-    return MenuItem('Forum', reverse('forum'), icon_name='date')
 
 @hooks.register('register_admin_menu_item')
 def register_certificate_menu_item():
