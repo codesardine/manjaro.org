e97392b5bc4462c629ee2e60a8a1bb659ac9d815
Vitor Lopes <vmnlop@gmail.com>
2022-07-11 10:36:34 +0100

    remove mix blend mode from links

--- manjaro/static/js/manjaro.js
+++ manjaro/static/js/manjaro.js
@@ -13,36 +13,6 @@ document.addEventListener('DOMContentLoaded', (event) => {
         menu.classList.remove("hidden")
     })
 
-    const cursor = document.getElementById("cursor");
-    document.addEventListener("mousemove", function(e) {
-        cursor.style.left = e.clientX + "px",
-        cursor.style.top = e.clientY + "px";
-    });
-
-
-    const cursorHoverA = document.querySelectorAll("a");
-
-    cursorHoverA.forEach(a => {
-        a.addEventListener("mouseover", function(e) {
-            if (!a.classList.contains("no-mouse-effects")) {              
-                if (a.classList.contains("cursor-difference")) {
-                    cursor.classList.add("cursor-difference")
-                } else if (a.classList.contains("cursor-saturation")) {
-                    cursor.classList.add("cursor-saturation")
-                }
-                if (!a.classList.contains("no-zoom")) {
-                    cursor.classList.add("enlarge")
-                }
-            }
-        })
-    });
-
-    cursorHoverA.forEach(a => {
-        a.addEventListener("mouseout", function(e) {
-            cursor.classList.remove("enlarge","cursor-difference","cursor-saturation","hidden")
-        })
-    });
-
     document.querySelectorAll('code').forEach((el) => {
         hljs.highlightElement(el);
     });
