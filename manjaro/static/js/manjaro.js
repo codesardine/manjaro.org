bb9f89f388abd23e3aebc2e9b2b0331fc93df978
Vitor Lopes <vmnlop@gmail.com>
2022-11-16 13:32:17 +0000

    [mobile] rework for new design

--- manjaro/static/js/manjaro.js
+++ manjaro/static/js/manjaro.js
@@ -1,20 +1,61 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    
-    const btnCloseMenu = document.getElementById("btn-close-menu")
-    const btnOpenMenu = document.getElementById("btn-open-menu")
+function toggleDmenu(ev) {
+    let menu = document.querySelector("#desktop-menu")
+    let footer = document.querySelector("#footer");
+    let screen = document.querySelector("#dim-screen")
 
-    btnCloseMenu.addEventListener("click", function() {
-        const menu = document.getElementById("menu")
-        menu.classList.add("translate-x-[-150%]")
-    })
+    menu.classList.toggle("translate-y-[-110%]")
+    footer.classList.toggle("translate-footer")
+    screen.classList.toggle("h-full")
+    screen.classList.toggle("w-full")
+    screen.classList.toggle("opacity-0")
+    screen.classList.toggle("opacity-50")
+  }
 
-    btnOpenMenu.addEventListener("click", function() {
-        const menu = document.getElementById("menu")
-        menu.classList.remove("translate-x-[-150%]")
-    })
+const tooltipIcon = "<i class='fa-solid fa-circle-info ml-2'></i>"
+ 
+document.addEventListener("DOMContentLoaded", (event) => {
 
-    document.querySelectorAll('code').forEach((el) => {
-        hljs.highlightElement(el);
+  const toggleMobileBtn = document.getElementById("toggle-mobile-btn")
+  toggleMobileBtn.addEventListener("click", function () {
+    const menu = document.getElementById("menu")
+    menu.classList.toggle("translate-x-[-150%]")
+
+  });
+
+  window.onclick = function (event) {
+    let leftSubmenu = document.querySelectorAll(".dropdown")
+    leftSubmenu.forEach(function (el) {
+      if (el.id === "submenu" + event.target.textContent.trim()) {
+        el.classList.toggle("hidden");
+      } else {
+        el.classList.add("hidden");
+      }
     });
+  };
 
-})
+  document.onkeyup = function (e) {
+    if (event.ctrlKey && e.which == 77) {
+      toggleDmenu();
+    }
+  }
+
+  tippy(".home-btn", {
+    content: "GO BACK HOME" + tooltipIcon,
+    allowHTML: true,
+  });
+
+  tippy("#download-btn", {
+    content: "GET MANJARO INSTALL MEDIA" + tooltipIcon,
+    allowHTML: true,
+  });
+
+  tippy("#menu-btn", {
+    content: "TOGGLE MENU - CTRL + M" + tooltipIcon,
+    allowHTML: true,
+  });
+
+  tippy("#top-btn", {
+    content: "BACK TO TOP" + tooltipIcon,
+    allowHTML: true,
+  });
+});
