db88076076716f6bc35f44f948e7483357bc660e
Vitor Lopes <vmnlop@gmail.com>
2023-03-22 01:03:54 +0000

    ajust secret key

--- manjaro/settings/base.py
+++ manjaro/settings/base.py
@@ -23,7 +23,7 @@ LOGGING = {
 }
 
 HARDWARE_NOISE = os.urandom(10)
-SECRET_KEY = get_random_secret_key() + HARDWARE_NOISE.hex() + str(time.time()) + get_random_secret_key()
+SECRET_KEY = get_random_secret_key() + HARDWARE_NOISE.hex() + str(time.time())
 
 PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 BASE_DIR = os.path.dirname(PROJECT_DIR)
