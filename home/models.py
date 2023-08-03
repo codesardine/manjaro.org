4a2f960bff26978a0d8816c0a8eae963c3aaa6b7
Vitor Lopes <vmnlop@gmail.com>
2023-08-03 16:03:26 +0100

    fix #128

--- home/models.py
+++ home/models.py
@@ -538,7 +538,7 @@ class CustomPage(Page):
     keywords = models.CharField(default='', blank=True, max_length=150)
 
     edit_handler = TabbedInterface([
-        ObjectList(Page.content_panels, heading=('Content')),
+        ObjectList(content_panels, heading=('Content')),
         ObjectList(Page.promote_panels, heading=('Promote')),
         ObjectList(Page.settings_panels, heading=('Settings')),
         YoastPanel(
