f708211d08cbc54e4caebaf5770d1f0603fc8392
Vitor Lopes <vmnlop@gmail.com>
2023-08-14 23:02:02 +0100

    catch browser_reload error

--- manjaro/urls.py
+++ manjaro/urls.py
@@ -28,8 +28,11 @@ urlpatterns = [
 if settings.DEBUG:
     from django.conf.urls.static import static
     from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-
-    urlpatterns += path("__reload__/", include("django_browser_reload.urls")),
+    try:
+        urlpatterns += path("__reload__/", include("django_browser_reload.urls")),
+    except ModuleNotFoundError as e:
+        print(e)
+    
     urlpatterns += staticfiles_urlpatterns()
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += path('__debug__/', include('debug_toolbar.urls')),
