e00fb73549a78d13b1f0f5084d91d86c26e50154
Vitor Lopes <vmnlop@gmail.com>
2023-10-27 18:02:29 +0100

    [aff] refractor models

--- advertising/models.py
+++ advertising/models.py
@@ -1,21 +1,24 @@
 from wagtail.admin.panels import FieldPanel
 from django.db import models
-from django.conf import settings
 from wagtail.admin.panels import (
     FieldPanel
 )
 
 
-class Advert(models.Model,):
+class Advert(models.Model):
     title = models.CharField(max_length=255, null=False, default="")
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, on_delete=models.SET_NULL)
     url = models.URLField(null=False, default="")
-    image = models.ImageField(null=False, default="")
     description = models.TextField(max_length=255, null=False, default="") 
+    image = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+',
+    )
 
     panels = [
         FieldPanel("title"),
-        FieldPanel("author"),
         FieldPanel("url"),
         FieldPanel("image"),
         FieldPanel("description"),
--- advertising/wagtail_hooks.py
+++ advertising/wagtail_hooks.py
@@ -6,16 +6,16 @@ from .models import Advert
 @register_snippet
 class Ads(SnippetViewSet):
     model = Advert
-    base_url_path = 'adverts'
-    menu_label = 'Advertisement' 
-    icon = 'decimal' 
+    base_url_path = 'affiliates'
+    menu_label = 'Affiliates' 
+    icon = 'clipboard-list' 
     menu_order = 200  
     add_to_settings_menu = False  
     exclude_from_explorer = False 
     add_to_admin_menu = True 
-    list_display = ('title', 'author')
-    list_filter = ('author',)
-    search_fields = ('title', 'author')
+    list_display = ('title', 'description')
+    list_filter = ('description',)
+    search_fields = ('title', 'description')
 
 
 
