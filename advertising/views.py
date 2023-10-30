e6c03769063fe6038ca267f2ecdd724ab4bea2e5
Vitor Lopes <vmnlop@gmail.com>
2023-10-30 11:22:28 +0000

    [aff] fix img renders

--- advertising/views.py
+++ advertising/views.py
@@ -2,20 +2,23 @@ from django.http import JsonResponse
 from .models import Advert
 from django.core.serializers import serialize
 import json
+from wagtail.images import get_image_model
 
 
 def ads_json(request):        
     ads = Advert.objects.all()
     serialized_data = serialize("json", ads)
     serialized_data = json.loads(serialized_data)
+    img_model = get_image_model()
 
     affiliates = []
     for ad in serialized_data:
         data = ad["fields"]
+        img = img_model.objects.filter(id=data["image"])[0]
         template = {
         "title": data["title"],
         "url": data["url"],
-        "img": data["image"],
+        "img": img.renditions.all()[1].url,
         "desc": data["description"],
         }
         affiliates.append(template)
