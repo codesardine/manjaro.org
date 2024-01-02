e5d27b58e09e6ea5e3d1c38c07f5ae9ca0d021c1
Vitor Lopes <vmnlop@gmail.com>
2024-01-02 17:35:50 +0000

    [packages] improve update message

--- compare/scheduler.py
+++ compare/scheduler.py
@@ -18,7 +18,7 @@ def start():
             start_date=today() + datetime.timedelta(seconds=5)
             )
     def update_pkgs():
-        print(f"Updating packages at: {today()}")
+        print(f"Checking for package updates: {today()}")
         update_x86_64(None)
         update_aarch64(None)
         print("")
